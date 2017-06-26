bbad277a3cd741c73f73f7caf1668b43330937de
张振峰 <xyls_ok@126.com>
2017-06-26 17:09:54 +0800

    隐藏掉计算接口

--- src/main/java/net/gddata/index/ui/ArticleSearch2Web.java
+++ src/main/java/net/gddata/index/ui/ArticleSearch2Web.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Map;
 
@@ -21,17 +22,21 @@ public class ArticleSearch2Web {
     @Autowired
     SearchService searchService;
 
+    @ApiIgnore
     @ApiOperation(value = "计算最相关(会写表慎重点击)", notes = "计算次相关(会写表慎重点击)")
     @RequestMapping(value = "/start", method = RequestMethod.GET)
     public void start() {
         searchService.start();
     }
 
+    @ApiIgnore
     @ApiOperation(value = "计算次相关(会写表慎重点击)", notes = "计算次相关(会写表慎重点击)")
     @RequestMapping(value = "/start2", method = RequestMethod.GET)
     public void start2() {
         searchService.start2();
     }
+
+
 
     @ApiOperation(value = "查看最相关占比", notes = "查看最相关占比")
     @RequestMapping(value = "/info", method = RequestMethod.GET)
@@ -39,7 +44,8 @@ public class ArticleSearch2Web {
         return searchService.info("z");
     }
 
-    @ApiOperation(value = "查看次关占比", notes = "查看次相关占比")
+
+    @ApiOperation(value = "查看次相关占比", notes = "查看次相关占比")
     @RequestMapping(value = "/info2", method = RequestMethod.GET)
     public Map info2() {
         return searchService.info("c");
